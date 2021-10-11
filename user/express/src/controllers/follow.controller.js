d098cc434d147f8b8be1cef61dd84968326fadd3
Ge Lee <gelee1255@gmail.com>
2021-10-11 21:21:02 +1100

    Create API for getting lists of followed/unfollowed users

--- user/express/src/controllers/follow.controller.js
+++ user/express/src/controllers/follow.controller.js
@@ -1,7 +1,7 @@
 const db = require("../database");
 
 /*
- * Add a new following user
+ * Add a new followed user
  * --------------------
  * success: connection
  */ 
@@ -16,7 +16,7 @@ exports.add = async (req, res) => {
 
 
 /*
- * Remove a following user
+ * Remove a followed user
  * --------------------
  * success: null
  */ 
@@ -29,4 +29,44 @@ exports.remove = async (req, res) => {
   });
 
   res.json(null);
+};
+
+
+/*
+ * Get all followed users
+ * --------------------
+ * success: users
+ */ 
+exports.followed = async (req, res) => {
+  // eager load all users
+  const users = await db.follow.findAll({
+    include: { model: db.user },
+    where: { followerId: req.query.followerId }
+  });
+
+  res.json(users); 
+};
+
+
+/*
+ * Get all unfollowed users
+ * --------------------
+ * success: users
+ */ 
+exports.unfollowed = async (req, res) => {
+  // get all followed users (eager load all users)
+  const followedUsers = await db.follow.findAll({
+    include: { model: db.user },
+    where: { followerId: req.query.followerId }
+  });
+
+  // extract the followed user id's
+  let followedUsersId = followedUsers.map(user => user.followingId);
+  followedUsersId.push(1); // add dummy user id
+  followedUsersId.push(req.query.followerId); // add self user id
+
+  // get all unfollowed users (i.e. users excluding followed users, dummy user and the user itself)
+  const unfollowedUsers = await db.user.findAll({ where: { id: { [db.Op.notIn]: followedUsersId } } });
+  
+  res.json(unfollowedUsers); 
 };
--- user/express/src/routes/follow.routes.js
+++ user/express/src/routes/follow.routes.js
@@ -2,11 +2,17 @@ module.exports = (express, app) => {
   const controller = require("../controllers/follow.controller.js");
   const router = express.Router();
 
-  // add a new following user
+  // add a new followed user
   router.get("/add", controller.add);
   
-  // remove a new following user
+  // remove a new followed user
   router.get("/remove", controller.remove);
+
+  // get all followed users
+  router.get("/followed", controller.followed);
+
+  // get all unfollowed users
+  router.get("/unfollowed", controller.unfollowed);
 
   // add routes to server
   app.use("/api/follow", router);
